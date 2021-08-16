7a4904343e8d6a8bc104b194bbb2d515a411a2d3
j-brn <me@bricker.io>
2021-08-16 22:06:39 +0200

    Add new bleeding edge build dependencies

--- overlay/looking-glass-client/default.nix
+++ overlay/looking-glass-client/default.nix
@@ -1,18 +1,24 @@
 { super, ... }:
 let
- version = "master-2021-07-17";
- checksum = "0fwmz0l1dcfwklgvxmv0galgj2q3nss90kc3jwgf6n80x27rsnhf";
+ version = "f7682c289ada4464b8da78d4f0c7d07652ad4d24";
+ checksum = "1z5cda59dpxspjm6sqil8rbk6nggnsslmhz1wvlbcbi2x1n07ifa";
 in
 {
    looking-glass-client = super.looking-glass-client.overrideAttrs (old: {
       version = "${version}";
-      buildInputs = old.buildInputs ++ [ super.xorg.libXi super.xorg.libXScrnSaver super.xorg.libXinerama ];
+      buildInputs = old.buildInputs ++ [
+        super.xorg.libXi
+        super.xorg.libXScrnSaver
+        super.xorg.libXinerama
+        super.xorg.libXcursor
+        super.xorg.libXpresent
+      ];
 
       src = super.fetchFromGitHub {
         owner = "gnif";
         repo = "LookingGlass";
-        rev = "HEAD";
-        sha256 = "${checksum}";
+        rev = version;
+        sha256 = checksum;
         fetchSubmodules = true;
       };
 
